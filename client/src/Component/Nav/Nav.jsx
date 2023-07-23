6c470fa6f4c1da73672714e7cd1d49639d2a303b
arjunn881 <arjunn881@gmail.com>
2023-07-23 23:24:16 +0530

    23/07  23:24

--- client/src/Component/Nav/Nav.jsx
+++ client/src/Component/Nav/Nav.jsx
@@ -1,19 +1,12 @@
-import React from 'react'
-import './Nav.css';
-
+import React from "react";
+import "./Nav.css";
 
 export const Nav = () => {
   return (
-    <div className='nav'>
+    <div className="nav">
+      <div className="nav-logo">SS</div>
 
-        <div className="nav-logo">
-                SS
-        </div>
-
-        <div className="nav-menu">
-                Menu
-        </div>
-    
+      <div className="nav-menu">Menu</div>
     </div>
-  )
-}
+  );
+};
